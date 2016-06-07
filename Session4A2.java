25a3f4e924a4c7f8348eab2659902a0d9fe785e7
Pratik Ratnaparkhi <pratikr557@gmail.com>
2016-06-07 19:11:35 +0530

    Add files via upload

---
+++ Session4A2.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class Session4A2 {
+
+	public static void main(String[] args) {
+		
+		//Sort an array and insert an element inside it. 
+		
+		
+		int[] array;
+		
+		array = new int[6];
+		
+		array[0]= 10;
+		array[1]= 20;
+		array[2]= 30;
+		array[3]= 40;
+		array[4]= 50;
+		array[5]= 60;
+		
+		
+		Arrays.sort( array );
+		
+		int i;
+		
+		for(i=0;i<array.length; i++){
+			
+		System.out.println("num: "+ array[i]);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+	}
+	}
